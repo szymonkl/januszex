30410ff5b93ed88df211e1389cfb8dce787389d9
Szymon <Szymon@SZYMON-KOMPUTER>
2016-11-10 23:06:40 +0100

    Add project files.

---
+++ JanuszSoftwareSolutions/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuszSoftwareSolutions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WPF taki fajny");
+            Console.ReadLine();
+        }
+    }
+}
